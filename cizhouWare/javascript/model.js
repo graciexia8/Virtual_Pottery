4488f6fc7e0fc9146a66343b6c6478f944cd6c08
Gracie Xia <graciexia@gmail.com>
2020-07-27 19:32:03 -0400

    created cizhou ware

---
+++ cizhouWare/javascript/model.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// Model "Object"
+window.simpleModel = function (name) {
+    const self = this;
+    self.name = name;
+    self.indices = [];
+	self.vertices = [];
+    self.normals = [];
+    self.textureCoords = [];
+	// self.uvCoord = uv;
+
+};
+
+// Model Object in Scene
+window.createModel = function (modelObj) {
+    console.log(modelObj)
+    const model = new simpleModel("flowerPot");
+    model.indices = [].concat.apply([], modelObj.meshes[0].faces);
+    // Vertex data
+    model.vertices = modelObj.meshes[0].vertices;
+    // White for Blanc de Chine
+	model.color = new Float32Array( [0.95, 0.96, 0.92, 1.0]); 
+    model.normals = modelObj.meshes[0].normals;
+    model.textureCoords = modelObj.meshes[0].texturecoords[0];
+    
+    return model;
+  };
+
+  // Light Model "Object"
+  window.lightModel = function (name, lightDirection, lightColor, ambientColor, ambientPercentage, lightShininess) {
+      const self = this;
+      self.name = name;
+      self.direction = lightDirection;
+      self.color = lightColor;
+      self.ambientColor = ambientColor;
+      self.ambientPercentage = ambientPercentage;
+      self.shininess = lightShininess;
+  }
+
+  // Create light model for scene
+  window.createLight = function () {
+    const lightDirection = new Float32Array([0.5, 1.8, 1.8]);
+    const lightColor = new Float32Array([1.0, 1.0, 0.9]);
+    const ambientColor = new Float32Array([0.5, 0.5, 0.9]);
+    const ambientPercentage = 0.65;
+    const lightShininess = 5.0;
+    const model = new lightModel("light", lightDirection, lightColor, ambientColor, ambientPercentage, lightShininess);
+
+    return model;
+  }
